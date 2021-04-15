0e40f3461493a0660f3ad7a2e14f41a7e8248981
Lars berglén <lars.berglen@ub.gu.se>
2021-04-15 15:24:55 +0200

    add solr and postgres to build compose file

--- build/push.sh
+++ build/push.sh
@@ -8,3 +8,5 @@ fi
 
 docker push docker.ub.gu.se/dliver-frontend:${GIT_REVISION_FRONTEND} && \
 docker push docker.ub.gu.se/dliver-backend:${GIT_REVISION_BACKEND} && \
+docker push docker.ub.gu.se/dliver-postgres:${GIT_REVISION_POSTGRES} && \
+docker push docker.ub.gu.se/dliver-solr:${GIT_REVISION_SOLR} && \
